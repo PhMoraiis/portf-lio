24ff8909a75176aa60d2d175f7ccd26ede9f0c39
Philipe Barbosa de Morais <liperapltda@gmail.com>
2022-07-19 22:55:23 -0300

    wait texts commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portf-lio
+## Portfólio pessoal
+
+Feito utilizando HTMl, CSS e JavaScript, com uso de algumas bibliotecas JS, como Vanilla Tilt.js.
+
+
---
+++ assets/scroll-reveal.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.text', {duration: 1000});
+
+sr.reveal('.logo-card', {duration: 1000});
+
+sr.reveal('.logo-card-linkedin', {duration: 1000});
+
+sr.reveal('.footer', {duration: 1000});
+
+sr.reveal('.menu-container', {duration: 1000});
